3ef9feddcbd1a6c42ee672884619590842348d06
RitikaB <rbalhara2004@gmail.com>
2025-02-08 16:54:30 +0530

    adding more programs

---
+++ first.java
@@ -0,0 +1,11 @@
+public class first {
+    public static void main(String[] args){
+        System.out.println("NAME     MAIL-ID          EMPLOYEE-CODE    PHONE");
+        System.out.println("Parul    parul@cse        p03160          25764728");
+        System.out.println("Sonia    sonia@cse        p03161          25764728");
+        System.out.println("Keshav   keshav@cse       p03162          25764729");
+        System.out.println("Jay      jay@cse          p03163          25764730");
+        System.out.println("Victor   victor@cse       p03164          25764731");
+
+    }
+}
